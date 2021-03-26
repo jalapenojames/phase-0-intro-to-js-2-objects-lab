80427b7886ea94b9cbf59d4c7f14426994210410
James <jaames.chen@gmail.com>
2021-03-26 15:13:03 -0400

    Completed Assignment (objects lab)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name: 'person1',
+    streetAddress: 'address'
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    return Object.assign({}, employeeObj, {[key]:value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    return Object.assign(employeeObj, {[key]: value});
+}
+
+function deleteFromEmployeeByKey(employeeObj, key) {
+    let newObj = Object.assign({},employeeObj);
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+   delete employeeObj[key];
+   return employeeObj;
+}
